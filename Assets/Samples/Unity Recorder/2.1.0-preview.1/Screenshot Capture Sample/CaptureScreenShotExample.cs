19117c47c533e03a96268570e985549327561442
youngfun2yf <yf.cn@outlook.com>
2020-01-28 23:56:45 +0800

    add sdk
    ironsourceSDK
    EasyTouchSDK
    FingerGestureSDK

---
+++ Assets/Samples/Unity Recorder/2.1.0-preview.1/Screenshot Capture Sample/CaptureScreenShotExample.cs
@@ -0,0 +1,65 @@
+#if UNITY_EDITOR
+
+using System.IO;
+using UnityEditor.Recorder;
+using UnityEditor.Recorder.Input;
+
+namespace UnityEngine.Recorder.Examples
+{
+    /// <summary>
+    /// This example shows how to setup a recording session via script.
+    /// To use this example. Simply add the CaptureScreenShotExample component to a GameObject.
+    /// 
+    /// Entering playmode will display a "Capture ScreenShot" button.
+    /// 
+    /// Recorded images are saved in [Project Folder]/SampleRecordings
+    /// </summary>
+    public class CaptureScreenShotExample : MonoBehaviour
+    {
+       RecorderController m_RecorderController;
+             
+       void OnEnable()
+       {
+           var controllerSettings = ScriptableObject.CreateInstance<RecorderControllerSettings>();
+           m_RecorderController = new RecorderController(controllerSettings);
+ 
+           var mediaOutputFolder = Path.Combine(Application.dataPath, "..", "SampleRecordings");
+
+           // Image
+           var imageRecorder = ScriptableObject.CreateInstance<ImageRecorderSettings>();
+           imageRecorder.name = "My Image Recorder";
+           imageRecorder.Enabled = true;
+           imageRecorder.OutputFormat = ImageRecorderSettings.ImageRecorderOutputFormat.PNG;
+           imageRecorder.CaptureAlpha = false;
+
+           string tmp = "image_" + DefaultWildcard.Take;
+           imageRecorder.OutputFile = Path.Combine(mediaOutputFolder, tmp);
+    
+           imageRecorder.imageInputSettings = new GameViewInputSettings
+           {
+               OutputWidth = 2160,
+               OutputHeight = 1080,
+           };
+    
+           // Setup Recording
+           controllerSettings.AddRecorderSettings(imageRecorder);
+           controllerSettings.SetRecordModeToSingleFrame(0);
+       }
+    
+        void OnGUI()
+        {
+            //if (GUI.Button(new Rect(10, 10, 150, 50), "Capture ScreenShot"))
+            //{
+            //    m_RecorderController.StartRecording();
+            //}
+        }
+
+        [ContextMenu("Record")]
+        void Record()
+        {
+            m_RecorderController.StartRecording();
+        }
+    }
+ }
+
+#endif
